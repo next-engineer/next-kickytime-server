46de30577939515e1e448534104512f51b0e6b43
hyeminleeee <wds5329@naver.com>
2025-08-09 20:50:15 +0900

    🏗️ setup: 프로젝트 초기 설정(close #1)

---
+++ src/test/java/com/kickytime/kickytime/KickytimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kickytime.kickytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickytimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
