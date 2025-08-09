d761dbd1f81126869d78a53a78e9dc9997eef84b
hyeminleeee <wds5329@naver.com>
2025-08-09 18:10:40 +0900

    🏗️ setup: Spring Boot 초기 설정

---
+++ src/main/java/com/kickytime/kickytime/KickytimeApplication.java
@@ -0,0 +1,13 @@
+package com.kickytime.kickytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickytimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickytimeApplication.class, args);
+	}
+
+}
