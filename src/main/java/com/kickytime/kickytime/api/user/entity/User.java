cc7d7487cdeae616759d21260e072bea9aa1ad5c
hyeminleeee <wds5329@naver.com>
2025-08-10 23:48:30 +0900

    ♻️  refactor: user entitiy refactoring(no logic change)

--- src/main/java/com/kickytime/kickytime/api/user/entity/User.java
+++ src/main/java/com/kickytime/kickytime/api/user/entity/User.java
@@ -1,12 +1,17 @@
 package com.kickytime.kickytime.api.user.entity;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.annotation.CreatedDate;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,10 +22,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @Column(nullable = false, unique = true)
+    @Email
+    @NotBlank
+    @Size(max = 255)
+    @Column(nullable = false, unique = true, length = 255)
     private String email;
 
-    @Column(nullable = false, unique = true)
+    @NotBlank
+    @Size(max = 50)
+    @Column(nullable = false, length = 50)
     private String nickname;
 
     @Column(nullable = false, unique = true, name = "cognito_sub")
@@ -40,7 +50,7 @@ public class User {
     @Enumerated(EnumType.STRING)
     private RankEnum rank;
 
-    @Column(name = "created_at")
     @CreatedDate
+    @Column(name = "created_at",updatable = false)
     private java.time.LocalDateTime createdAt;
 }
