0f7611c298fbe3c718e3c1c6a7e22802e1941eeb
ImJWoo <jeongu8807@daum.net>
2025-08-11 18:56:45 +0900

    "🔧 config: SecurityConfig, WebConfig @Configuration 추가

--- src/main/java/com/nextcloudlab/kickytime/config/SecurityConfig.java
+++ src/main/java/com/nextcloudlab/kickytime/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.nextcloudlab.kickytime.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
