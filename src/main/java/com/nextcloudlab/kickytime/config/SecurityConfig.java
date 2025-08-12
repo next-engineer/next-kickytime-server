2f34eb0f2cafc488b5eba76d4be8376d5e30847c
hyeminleeee <wds5329@naver.com>
2025-08-12 15:33:30 +0900

    ✨ feat(user): jwt 자동 검증 설정 추가

--- src/main/java/com/nextcloudlab/kickytime/config/SecurityConfig.java
+++ src/main/java/com/nextcloudlab/kickytime/config/SecurityConfig.java
@@ -26,6 +26,9 @@ public class SecurityConfig {
                                         .authenticated()
                                         .anyRequest()
                                         .permitAll())
+                .oauth2ResourceServer(
+                        oauth2 ->
+                                oauth2.jwt(Customizer.withDefaults()))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
