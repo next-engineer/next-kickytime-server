a66873a3cc920690fe1fcd660741cf2fb779814a
hyeminleeee <wds5329@naver.com>
2025-08-13 12:57:15 +0900

    ✨ feat(user): accessToken 디코더 추가

--- src/main/java/com/nextcloudlab/kickytime/config/SecurityConfig.java
+++ src/main/java/com/nextcloudlab/kickytime/config/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.nextcloudlab.kickytime.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
@@ -26,9 +33,20 @@ public class SecurityConfig {
                                         .authenticated()
                                         .anyRequest()
                                         .permitAll())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2.jwt(jwt -> jwt.decoder(accessTokenDecoder())))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    @Bean
+    JwtDecoder accessTokenDecoder() {
+        NimbusJwtDecoder decoder = JwtDecoders.fromIssuerLocation(issuerUri);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
+        OAuth2TokenValidator<Jwt> tokenUserIsAccess = new TokenUseValidator("access");
+        decoder.setJwtValidator(
+                new DelegatingOAuth2TokenValidator<>(withIssuer, tokenUserIsAccess));
+        return decoder;
     }
 }
