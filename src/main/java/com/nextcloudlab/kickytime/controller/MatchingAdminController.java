ea6a34ea9c684677029f8981f47b57689bd3985d
taeyeon <rnemrdud1@gmail.com>
2025-08-13 16:11:26 +0900

    🚨 fix(test): 테스트 실패 원인 수정

--- src/main/java/com/nextcloudlab/kickytime/controller/MatchingAdminController.java
+++ src/main/java/com/nextcloudlab/kickytime/controller/MatchingAdminController.java
@@ -1,12 +1,12 @@
 package com.nextcloudlab.kickytime.controller;
 
-import com.nextcloudlab.kickytime.service.MatchingService;
-
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import com.nextcloudlab.kickytime.service.MatchingService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/matches")
