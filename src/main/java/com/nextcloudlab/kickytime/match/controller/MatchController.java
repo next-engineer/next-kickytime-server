6f55b9a318b260d34d02771ae9843e5830c94e1f
ImJWoo <jeongu8807@daum.net>
2025-08-14 02:12:56 +0900

    ✨ feat(auth): 나의 매치 목록 확인 controller 추가

--- src/main/java/com/nextcloudlab/kickytime/match/controller/MatchController.java
+++ src/main/java/com/nextcloudlab/kickytime/match/controller/MatchController.java
@@ -1,8 +1,14 @@
 package com.nextcloudlab.kickytime.match.controller;
 
+import java.util.Collections;
 import java.util.List;
 
+import com.nextcloudlab.kickytime.match.dto.MyMatchesResponse;
+import com.nextcloudlab.kickytime.match.service.MatchParticipantService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import com.nextcloudlab.kickytime.match.service.MatchService;
@@ -11,14 +17,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/matches")
+@RequiredArgsConstructor
 @Tag(name = "경기 관련", description = "경기 개설, 참여, 취소 관련 API")
 public class MatchController {
 
     private final MatchService matchService;
-
-    public MatchController(MatchService matchService) {
-        this.matchService = matchService;
-    }
+    private final MatchParticipantService matchParticipantService;
 
     // 전체 경기 목록 조회
     @GetMapping
@@ -47,4 +51,13 @@ public class MatchController {
         matchService.leaveMatch(matchId, userId);
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/me")
+    public ResponseEntity<MyMatchesResponse> getMyMatches(@AuthenticationPrincipal Jwt jwt) {
+        String cognitoSub = jwt.getClaimAsString("sub");
+        MyMatchesResponse response = matchParticipantService.getMyParticipations(cognitoSub);
+        return ResponseEntity.ok(response);
+    }
+
+
 }
