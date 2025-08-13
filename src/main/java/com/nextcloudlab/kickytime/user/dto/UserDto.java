a329a597d1e0270014d7ed5814dfa3e59ac494dd
hyeminleeee <wds5329@naver.com>
2025-08-14 08:58:47 +0900

    ✨ feat(user): userDto createdAt 추가

--- src/main/java/com/nextcloudlab/kickytime/user/dto/UserDto.java
+++ src/main/java/com/nextcloudlab/kickytime/user/dto/UserDto.java
@@ -1,11 +1,17 @@
 package com.nextcloudlab.kickytime.user.dto;
 
-import com.nextcloudlab.kickytime.user.entity.User;
-
 import java.time.LocalDateTime;
 
+import com.nextcloudlab.kickytime.user.entity.User;
+
 public record UserDto(
-        Long id, String email, String nickname, String imageUrl, String role, String rank, LocalDateTime createdAt) {
+        Long id,
+        String email,
+        String nickname,
+        String imageUrl,
+        String role,
+        String rank,
+        LocalDateTime createdAt) {
     public static UserDto from(User user) {
         return new UserDto(
                 user.getId(),
@@ -14,7 +20,6 @@ public record UserDto(
                 user.getImageUrl(),
                 user.getRole().name(),
                 user.getRank().name(),
-                user.getCreatedAt()
-        );
+                user.getCreatedAt());
     }
 }
